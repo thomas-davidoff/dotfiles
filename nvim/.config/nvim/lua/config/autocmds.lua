ed82323bc7ca0dabdde30a9283a241eba25a0787
Thomas Davidoff <thomas.davidoff@gmail.com>
2025-12-04 14:06:26 -0500

    Add nvim autocmds to yank file paths of buffers.

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -19,4 +19,20 @@ vim.api.nvim_create_user_command('Config', function()
   vim.cmd 'tabnew ~/.config/nvim/init.lua'
 end, {})
 
--- Add more autocommands here as needed
+-- Yank absolute current file path to clipboard
+vim.keymap.set('n', '<leader>yp', function()
+  vim.fn.setreg('+', vim.fn.expand '%:p')
+  print('Yanked: ' .. vim.fn.expand '%:p')
+end, { desc = 'Yank absolute path' })
+
+-- Yank relative current file path to clipboard
+vim.keymap.set('n', '<leader>yr', function()
+  vim.fn.setreg('+', vim.fn.expand '%')
+  print('Yanked: ' .. vim.fn.expand '%')
+end, { desc = 'Yank relative path' })
+
+-- Yank just filename
+vim.keymap.set('n', '<leader>yf', function()
+  vim.fn.setreg('+', vim.fn.expand '%:t')
+  print('Yanked: ' .. vim.fn.expand '%:t')
+end, { desc = 'Yank filename' })
