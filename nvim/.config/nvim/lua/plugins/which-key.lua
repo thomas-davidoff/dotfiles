d5151de6743902e34e57045874e371ece972519f
Thomas Davidoff <thomas.davidoff@gmail.com>
2025-12-04 19:02:53 -0500

    Remove which-key delay.

--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -7,6 +7,7 @@ return {
   'folke/which-key.nvim',
   event = 'VimEnter',
   opts = {
+    delay = 0, -- Set delay to 0
     icons = {
       mappings = vim.g.have_nerd_font,
       keys = vim.g.have_nerd_font and {} or {
